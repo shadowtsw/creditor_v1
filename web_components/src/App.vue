f6c01fc1f9249b1692b47b50362637b2fc2d836b
gerrul3r <shadow_tsw@web.de>
2021-07-21 00:13:27 +0200

    first initial

--- web_components/src/App.vue
+++ web_components/src/App.vue
@@ -1,30 +1,41 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <h1>h1: {{ myTitle }} - Chakra</h1>
+    <h2>h2: Test - Chakra</h2>
+    <h3>h3: Test - Baloo Da 2</h3>
+    <h4>h4: Test - Baloo Da 2</h4>
+    <p variant="mukta">p: Test -'Mukta'</p>
+    <span variant="mukta">span: test 'Mukta'</span>
+    <div class="text">
+      <p variant="bai">p: Test -'Bai'</p>
+      <span variant="bai">span: test 'Bai'</span>
+    </div>
+    <p></p>
+    <button variant="neutral">Neutral</button>
+    <button variant="success">Success</button>
+    <button variant="warning">Warning</button>
+    <button variant="danger">Danger</button>
   </div>
-  <router-view />
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup(props, context) {
+    return {
+      myTitle: 'My biggest Title',
+    };
+  },
+});
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import './styles/import-libs.scss';
+</style>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style scoped lang="scss">
+* {
+  font-size: 2rem;
 }
 </style>
